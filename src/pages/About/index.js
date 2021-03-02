3f0a4c05978d30e0f860c25d2b4e80b784104865
Annie Kwan <akwan.mtl@gmail.com>
2021-03-01 22:49:15 -0500

    not working

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -14,7 +14,7 @@ function About () {
       I'm a full stack web developer with a vast experience in customer support and troubleshooting. Originally from Montréal, I'm currently based in Toronto. Currently enrolled in Coding Bootcamp at UofT, I have gained skills in full stack development and highly proficient in React, Javascript, NodeJS, APIs, Express and MySQL.
       </div>
       <div className='profile-bio'>
-      I have always had an interest in developing applications that simplify tasks. Prior to the Bootcamp, I have built a website with a goal to make language-exchange more interactive and a Matlab-based GUI to visualize and analyze data. Now, I have gained more knowledge and skills and have built applications geared towards aspiring cooks and book lovers to further expand their hobbies. You can find all my projects <span><Link to="/projects">here</Link></span>
+      I have always had an interest in developing applications that simplify tasks. Prior to the Bootcamp, I have built a website with a goal to make language-exchange more interactive and a Matlab-based GUI to visualize and analyze data. Now, I have gained more knowledge and skills and have built applications geared towards aspiring cooks and book lovers to further expand their hobbies. You can find all my projects <span><Link to="/portfolio/projects">here</Link></span>
       </div>
       <div className='profile-bio'>
       When you don't find me working, you can find me in the kitchen baking sweet treats or outside enjoying a nice long walk. I also enjoy playing video games, in particular, Smash Bros and I'm always looking for a friendly competition.
