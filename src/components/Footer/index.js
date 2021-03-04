758cf14b7a5c6396d15a6026d70bc292c9551488
Annie Kwan <akwan.mtl@gmail.com>
2021-03-03 21:50:35 -0500

    changed font and fixed footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
+import cvIcon from "../../assets/cv.png";
 
 import './footer.css'
 function Footer () {
@@ -29,9 +30,9 @@ function Footer () {
           </a>
         </div>
 
-        <div className="icon icon-cv">
+        <div className="icon">
           <a href="https://docs.google.com/document/d/1MiEcZn3aN-DQYndVq2tz4nKPyF9yDeB8itE_QbVqQlA/edit?usp=sharing" target="_blank"  rel="noopener noreferrer" className="cv">
-          CV
+          <img src={cvIcon} alt="CV" className="cvImg"/>
           </a>
         </div>
       </div>
