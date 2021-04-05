28a7ae38f8934d886afddf86faa6af31b4f136d5
Annie Kwan <akwan.mtl@gmail.com>
2021-04-04 21:01:19 -0400

    updated

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import profilepic from "../../assets/profilepic.png";
-import './home.css';
 import {Animated} from "react-animated-css";
+import './home.css';
+import Typed from 'react-typed';
 
 function Home () {
   return(
     <div>
+      
     <Animated animationIn="pulse" animationOut="fadeOut" isVisible={true}>
     
       <div className='text-center'>
@@ -14,7 +16,17 @@ function Home () {
       <img src={profilepic} alt="Annie Kwan" className="profile-pic img-fluid"></img>
       </div>
       <div className='home-bio'>
-      I'm a full stack web developer. 
+      I'm a full stack web developer.  
+      </div>
+
+      
+      <div className='skills'>
+      <Typed
+          strings={["HTML" ,"CSS", "JavaScript", "JQuery" , "Bootstrap" ,"Node", "Express", "React", "MySQL", "MongoDB"]}
+          typeSpeed={50}
+          backSpeed={60}
+          loop
+        />
       </div>
     </Animated>
     </div>
